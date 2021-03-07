716b7e003bcc9f9392ddb763b5e9e8086af09efb
ScriptHound <grinkiko@gmail.com>
2021-03-07 13:23:36 +0300

    Added Path to yaml_processing

--- config/yaml_processing.py
+++ config/yaml_processing.py
@@ -1,7 +1,9 @@
 import yaml
+from pathlib import Path
 
 
 def get_config(filepath):
+    filepath = Path('.') / filepath
     with open(filepath, 'r', encoding='utf-8') as f:
         data = yaml.load(f, Loader=yaml.Loader)
         return data
