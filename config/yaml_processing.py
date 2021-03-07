2e0e629c5474efb68a7b20e9133c2a4f6710c120
ScriptHound <grinkiko@gmail.com>
2021-03-07 13:31:37 +0300

    path Absolute Path

--- config/yaml_processing.py
+++ config/yaml_processing.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 
 def get_config(filepath):
-    filepath = Path('.') / filepath
-    with open(filepath, 'r', encoding='utf-8') as f:
+    filepath: Path = Path('.') / filepath
+    with open(filepath.absolute(), 'r', encoding='utf-8') as f:
         data = yaml.load(f, Loader=yaml.Loader)
         return data
